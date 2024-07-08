cc2aed5efdb1fa3637c5b3d1586501f4b448951f
MindW1n <ivan.prrrosvirov@gmail.com>
2024-07-08 19:35:33 +0300

    Update makeApiRequest

--- libs/makeApiRequest.js
+++ libs/makeApiRequest.js
@@ -1,14 +1,15 @@
-const rs = {}
+const rs = {}; const rjs = {}
 let id = 1
 window.receiveSignalFromCpp = function(result) {
 	const {reqId, ...rest} = result
-	const r = rs[reqId]
-	delete rs[reqId]
+	const [r, rj] = [rs[reqId], rjs[reqId]]
+	delete rs[reqId]; delete rjs[reqId]
+	if(result.error) rj({message: result.error})
 	r(rest)
 }
 export default function makeApiRequest(path, body = {}) {
-	return new Promise(r => {
+	return new Promise((r, rj) => {
 		window.webkit.messageHandlers.cppSignal.postMessage(JSON.stringify({path, body, reqId: id}))
-		rs[id++] = r
+		rs[id] = r; rjs[id++] = rj
 	})
 }
