3b0cd6ce2cbabf12efa389c053418e36447d8614
MindW1n <ivan.prrrosvirov@gmail.com>
2024-07-08 19:34:43 +0300

    Update backend mjs

--- app/api/backend.mjs
+++ app/api/backend.mjs
@@ -1,11 +1,13 @@
 import {createReadStream, createWriteStream} from "fs"
 import {exec} from "child_process"
 const pipe = [createReadStream(null, {fd: Number(process.argv[2])}), createWriteStream(null, {fd: Number(process.argv[3])})]
-exec("systemctl is-active postgresql.service", (error, stdout) => {
-	if(stdout.trim() == "active") return
-	exec("systemctl start postgresql.service")
-})
+// Uncomment this if you use postgres as I do
+// exec("systemctl is-active postgresql.service", (error, stdout) => {
+// 	if(stdout.trim() == "active") return
+// 	exec("systemctl start postgresql.service")
+// })
 pipe[0].on("data", async message => {
 	const {path, body, reqId} = JSON.parse(message)
-	pipe[1].write(JSON.stringify({reqId, ...await import("./routes/" + path + ".mjs").then(m => m.default(body))}))
+	try{pipe[1].write(JSON.stringify({reqId, ...await import("./routes/" + path + ".mjs").then(m => m.default(body))}))}
+	catch(e) {pipe[1].write(JSON.stringify({reqId, error: "Route not found"}))}
 })
